66e86ccc7167f8971b63b543983d92e157cb01c6
Sainjargal Bolortsetseg <azure.blr24@gmail.com>
2024-09-06 13:10:32 +0900

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,33 @@ function menuOnClick(){
 	document.getElementById("nav").classList.toggle("change");
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+function menuToggle() {
+	var target = document.querySelector('.menu-label');
+	var scrollWrap = document.querySelector('.menu');
+	var body = document.body;
+	var nav = document.querySelector('.nav');
+	  menuToggle.scrollTop = 0;
+  
+	  target.addEventListener('click',onClick);
+  
+	  function onClick(e) {      
+		var scrollTop = window.scrollTop;
+  
+		if (target.classList.contains("is-open")) {
+		  target.classList.remove('is-open');
+		  nav.classList.remove('on');
+		  scrollWrap.style.cssText = ''; 
+		  body.style.cssText = '';
+		} else {
+		  menuToggle.scrollTop = window.scrollY;
+		  body.style.overflow = 'hidden'
+		  scrollWrap.style.position = 'fixed';
+		  scrollWrap.style.width = '100%';
+		  scrollWrap.style.top = -menuToggle.scrollTop + 'px';
+		  target.classList.add('is-open');
+		  nav.classList.add('on');
+		}
+	  }
+  }
+  menuToggle();
